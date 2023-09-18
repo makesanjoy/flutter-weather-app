769a0a2f5da96488c07a20f8dd50751d9e2c0327
Sanjay Singha <sanjay.singha@karkinos.in>
2023-09-18 23:30:12 +0530

    setting url name

--- lib/secret.dart
+++ lib/secret.dart
@@ -1,1 +1,3 @@
-const openWeatherApiKey = "YOUR_API_KEY";
+const openWeatherApiKey = "https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=";
+
+///CREATE API KEY FROM THE ABOVE URL
